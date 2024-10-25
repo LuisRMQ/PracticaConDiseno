7a94cf6073fb1f9475efbe553c706fd4db3d5e15
VictorG <55268750+victoradan2015@users.noreply.github.com>
2024-10-24 18:27:28 -0600

    Agrega nuevo disenio para interfaz de configuracion

--- Cuenta.php
+++ Cuenta.php
@@ -20,100 +20,150 @@
 <body>
     <main>
         <h1>Configuración</h1>
-        <div id="config-form">
-            <label for="tituloInput">Título de la App:</label>
-            <input type="text" id="tituloInput" /><br />
+        <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form>
+                    <div class="form-group row">
+                        <label for="tituloInput" class="col-sm-4 col-form-label text-right">Título de la App:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="tituloInput">
+                        </div>
+                    </div>
 
-            <label for="bienvenidaInput">Mensaje de Bienvenida:</label>
-            <input type="text" id="bienvenidaInput" /><br />
+                    <div class="form-group row">
+                        <label for="bienvenidaInput" class="col-sm-4 col-form-label text-right">Mensaje de Bienvenida:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="bienvenidaInput">
+                        </div>
+                    </div>
 
-            <br>
-            <hr>
-            <br>
+                    <div class="form-group row">
+                        <label for="homeInput" class="col-sm-4 col-form-label text-right">Mensaje Home:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="homeInput">
+                        </div>
+                    </div>
 
-            <label for="homeInput">Mensaje Home:</label>
-            <input type="text" id="homeInput" /><br />
+                    <div class="form-group row">
+                        <label for="cambioInput" class="col-sm-4 col-form-label text-right">Mensaje Cambio:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="cambioInput">
+                        </div>
+                    </div>
 
-            <label for="cambioInput">Mensaje Cambio:</label>
-            <input type="text" id="cambioInput" /><br />
+                    <div class="form-group row">
+                        <label for="operacionesInput" class="col-sm-4 col-form-label text-right">Mensaje Operaciones:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="operacionesInput">
+                        </div>
+                    </div>
 
-            <label for="operacionesInput">Mensaje Operaciones:</label>
-            <input type="text" id="operacionesInput" /><br />
+                    <div class="form-group row">
+                        <label for="configInput" class="col-sm-4 col-form-label text-right">Mensaje Configuración:</label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control" id="configInput">
+                        </div>
+                    </div>
 
-            <label for="configInput">Mensaje Configuración:</label>
-            <input type="text" id="configInput" /><br />
+                    <div class="form-group row">
+                        <label for="cambioIconInput" class="col-sm-4 col-form-label text-right">Icono Cambio:</label>
+                        <div class="col-sm-8">
+                            <select class="form-control" id="cambioIconInput" onchange="updateIconPreview('cambioIconPreview', this.value)">
+                                <option value="fa-solid fa-arrow-right">Cambio</option>
+                                <option value="fa-solid fa-sync">Actualizar</option>
+                                <option value="fa-solid fa-exchange-alt">Intercambiar</option>
+                            </select>
+                            <i id="cambioIconPreview" class="icon-preview fa-solid fa-arrow-right"></i>
+                        </div>
+                    </div>
 
-            <br>
-            <hr>
-            <br>
+                    <div class="form-group row">
+                        <label for="operacionesIconInput" class="col-sm-4 col-form-label text-right">Icono Operaciones:</label>
+                        <div class="col-sm-8">
+                            <select class="form-control" id="operacionesIconInput" onchange="updateIconPreview('operacionesIconPreview', this.value)">
+                                <option value="fa-solid fa-cogs">Operaciones</option>
+                                <option value="fa-solid fa-tools">Herramientas</option>
+                                <option value="fa-solid fa-clipboard-list">Lista</option>
+                            </select>
+                            <i id="operacionesIconPreview" class="icon-preview fa-solid fa-cogs"></i>
+                        </div>
+                    </div>
 
-            <label for="cambioIconInput">Icono Cambio:</label>
-            <select id="cambioIconInput" onchange="updateIconPreview('cambioIconPreview', this.value)">
-                <option value="fa-solid fa-arrow-right">Cambio</option>
-                <option value="fa-solid fa-sync">Actualizar</option>
-                <option value="fa-solid fa-exchange-alt">Intercambiar</option>
-                <!-- Agrega más opciones según sea necesario -->
-            </select>
-            <i id="cambioIconPreview" class="icon-preview fa-solid fa-arrow-right"></i><br />
+                    <div class="form-group row">
+                        <label for="homeIconInput" class="col-sm-4 col-form-label text-right">Icono Home:</label>
+                        <div class="col-sm-8">
+                            <select class="form-control" id="homeIconInput" onchange="updateIconPreview('homeIconPreview', this.value)">
+                                <option value="fa-solid fa-house">Home</option>
+                                <option value="fa-solid fa-home">Inicio</option>
+                                <option value="fa-solid fa-hotel">Hotel</option>
+                            </select>
+                            <i id="homeIconPreview" class="icon-preview fa-solid fa-house"></i>
+                        </div>
+                    </div>
 
-            <label for="operacionesIconInput">Icono Operaciones:</label>
-            <select id="operacionesIconInput" onchange="updateIconPreview('operacionesIconPreview', this.value)">
-                <option value="fa-solid fa-cogs">Operaciones</option>
-                <option value="fa-solid fa-tools">Herramientas</option>
-                <option value="fa-solid fa-clipboard-list">Lista</option>
-                <!-- Agrega más opciones según sea necesario -->
-            </select>
-            <i id="operacionesIconPreview" class="icon-preview fa-solid fa-cogs"></i><br />
+                    <div class="form-group row">
+                        <label for="configIconInput" class="col-sm-4 col-form-label text-right">Icono Configuración:</label>
+                        <div class="col-sm-8">
+                            <select class="form-control" id="configIconInput" onchange="updateIconPreview('configIconPreview', this.value)">
+                                <option value="fa-solid fa-cog">Configuración</option>
+                                <option value="fa-solid fa-sliders-h">Ajustes</option>
+                                <option value="fa-solid fa-cogs">Rueda dentada</option>
+                            </select>
+                            <i id="configIconPreview" class="icon-preview fa-solid fa-cog"></i>
+                        </div>
+                    </div>
 
-            <label for="homeIconInput">Icono Home:</label>
-            <select id="homeIconInput" onchange="updateIconPreview('homeIconPreview', this.value)">
-                <option value="fa-solid fa-house">Home</option>
-                <option value="fa-solid fa-home">Inicio</option>
-                <option value="fa-solid fa-hotel">Hotel</option>
-                <!-- Agrega más opciones según sea necesario -->
-            </select>
-            <i id="homeIconPreview" class="icon-preview fa-solid fa-house"></i><br />
+                    <div class="form-group row">
+                        <label for="logoFileInput" class="col-sm-4 col-form-label text-right">Seleccionar Logo:</label>
+                        <div class="col-sm-8">
+                            <input type="file" class="form-control" id="logoFileInput" accept="image/*">
+                        </div>
+                    </div>
 
-            <label for="configIconInput">Icono Configuración:</label>
-            <select id="configIconInput" onchange="updateIconPreview('configIconPreview', this.value)">
-                <option value="fa-solid fa-cog">Configuración</option>
-                <option value="fa-solid fa-sliders-h">Ajustes</option>
-                <option value="fa-solid fa-cogs">Rueda dentada</option>
-                <!-- Agrega más opciones según sea necesario -->
-            </select>
-            <i id="configIconPreview" class="icon-preview fa-solid fa-cog"></i><br />
+                    <div class="form-group row justify-content-center">
+                        <div class="col-sm-12 text-center">
+                            <img id="logoPreview" src="" alt="Logo Preview" class="img-fluid">
+                        </div>
+                    </div>
 
-            <br>
-            <hr>
-            <br>
+                    <div class="form-group row justify-content-center">
+                        <button class="btn btn-primary" type="button" onclick="guardarConfiguracion()">Guardar Cambios</button>
+                    </div>
 
-            <label for="logoInput" style="display: none;">URL del Logo:</label>
-            <input type="text" id="logoInput" style="display: none;"/><br />
-
-
-            <br>
-            <hr>
-            <br>
-            <label for="logoFileInput">Seleccionar Logo:</label>
-            <input type="file" id="logoFileInput" accept="image/*" /><br />
-            <br>
-            <div class="center-image-container">
-                <img id="logoPreview" src="" alt="Logo Preview">
+                    <input type="text" id="logoInput" style="display: none;">
+                </form>
             </div>
-
-            <button onclick="guardarConfiguracion()">Guardar Cambios</button>
         </div>
-    </main>
 
-    <div id="footer-bar" class="footer-bar-6">
-        <a href="index.php" data-script="js/app.js"><i id="homeIcon"></i><span id="home"></span></a>
-        <a href="TipoCambio.php" data-script="js/TipoCambio.js"><i id="tpcambioIcon"></i><span id="tpcambio"></span></a>
-        <a href="Operaciones.php" data-script="js/Operaciones.js"><i id="operacionesIcon"></i><span id="operaciones"></span></a>
-        <a href="Cuenta.php" data-script="js/Configuracion.js"><i id="configuIcon"></i><span id="configu"></span></a>
+        <div id="footer-bar" class="footer-bar-6">
+            <a href="index.php" data-script="js/app.js">
+                <i class="fa-solid fa-house"></i>
+                <span id="home">Home</span>
+            </a>
+            <a href="TipoCambio.php" data-script="js/TipoCambio.js">
+                <i class="fa-solid fa-arrow-right"></i>
+                <span id="tpcambio">Tipo de Cambio</span>
+            </a>
+            <a href="Operaciones.php" data-script="js/Operaciones.js">
+                <i class="fa-solid fa-cogs"></i>
+                <span id="operaciones">Operaciones</span>
+            </a>
+            <a href="Cuenta.php" data-script="js/Configuracion.js">
+                <i class="fa-solid fa-cog"></i>
+                <span id="configu">Configuración</span>
+            </a>
+        </div>
+        
     </div>
 
-    <!-- Asegúrate de que tu archivo de JavaScript se llame "Configuracion.js" -->
-    <script src="js/Configuracion.js"></script>
+    </main>
+
+    
 </body>
 
+
+
 </html>
+
+<script src="js/Configuracion.js"></script>
